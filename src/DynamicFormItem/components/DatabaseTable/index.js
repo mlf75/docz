e78347c7caa2415552e0afc7c2f27097302b209c
lsq <605550967@qq.com>
2020-09-30 15:35:44 +0800

    文档初版

--- src/DynamicFormItem/components/DatabaseTable/index.js
+++ src/DynamicFormItem/components/DatabaseTable/index.js
@@ -4,15 +4,19 @@
  * @description
  */
 
-import React, { Component, useState, useEffect, useCallback, Fragment } from 'react';
+import React, {
+  Component,
+  useState,
+  useEffect,
+  useCallback,
+  Fragment,
+} from 'react';
 import { Input, Form } from 'antd';
-import {
-  PlusOutlined
-} from '@ant-design/icons';
-import TableFields from '../TableFields'
+import { PlusOutlined } from '@ant-design/icons';
+import TableFields from '../TableFields';
+import DatabaseTableModal from './DatabaseTableModal';
 
 export default function DatabaseTable(props) {
-
   const {
     options,
     label,
@@ -23,13 +27,85 @@ export default function DatabaseTable(props) {
     form,
     style,
     hidden,
-    colon
+    colon,
   } = props;
 
-  const [visible, setVisible] = useState(false)
-  const [selectedRowKeys, setSelectedRowKeys] = useState([])
+  const [visible, setVisible] = useState(false);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([formData.tableXmlid]);
 
-  const dbname = formData.dbname || null
+  const dbname = formData.dbname || null;
+
+  const handleTableSelect = useCallback((key, rows) => {
+    if (!rows.length) {
+      return;
+    }
+    setVisible(false);
+    setSelectedRowKeys(key);
+    const data = {
+      tableXmlid: rows[0].rowcode,
+      tableFields: [
+        {
+          colcnname: '雇工标识',
+          colname: 'EMPLOYEE_ID',
+          colxmlid: '0c63c164891d325cf7a4a9cacaa26b25',
+          datatype: 'DECIMAL',
+          focus: 0,
+          length: null,
+          tablexmlid: 'a5b161ecb2dcada936980cc56cefce58',
+        },
+        {
+          colcnname: '雇工名称',
+          colname: 'EMPLOYEE_NAME',
+          colxmlid: '0eeb28ba5f0c6c943d4a2f616073cc70',
+          datatype: 'VARCHAR',
+          focus: 0,
+          length: '20',
+          tablexmlid: 'd25fdb1cf94f604ec22a8514221050e4',
+        },
+        {
+          colcnname: '身份卡片',
+          colname: 'IDENTITY_CARD',
+          colxmlid: '89ae30dccabe3d0396aee54cf22de96f',
+          datatype: 'VARCHAR',
+          focus: 0,
+          length: '18',
+          tablexmlid: 'd25fdb1cf94f604ec22a8514221050e4',
+        },
+        {
+          colcnname: '电子邮箱',
+          colname: 'EMAIL',
+          colxmlid: '9012d19a315ec61a8105f57d97923390',
+          datatype: 'VARCHAR',
+          focus: 0,
+          length: '50',
+          tablexmlid: 'd25fdb1cf94f604ec22a8514221050e4',
+        },
+        {
+          colcnname: '座机',
+          colname: 'PHONE',
+          colxmlid: '5846bd4d4f762cc02b10fe5e2209e196',
+          datatype: 'VARCHAR',
+          focus: 0,
+          length: '15',
+          tablexmlid: 'd25fdb1cf94f604ec22a8514221050e4',
+        },
+        {
+          colcnname: '日期号码',
+          colname: 'DATE_NO',
+          colxmlid: 'b6099b83c71ab5132c33af92eca03dd7',
+          datatype: 'STRING',
+          focus: 0,
+          length: null,
+          tablexmlid: 'd25fdb1cf94f604ec22a8514221050e4',
+        },
+      ],
+    };
+    form.setFieldsValue({
+      tableName: rows[0].rowname,
+      tableXmlid: rows[0].rowcode,
+    });
+    props.getValue(data);
+  }, []);
 
   return (
     <Fragment>
@@ -44,16 +120,21 @@ export default function DatabaseTable(props) {
       >
         <Input
           disabled
-          placeholder='请选择表名称'
-          addonAfter={<PlusOutlined 
-            onClick={() => setVisible(true)}
-             />}
+          placeholder="请选择表名称"
+          addonAfter={<PlusOutlined onClick={() => setVisible(true)} />}
         />
       </Form.Item>
-      <TableFields
-        formData={formData}
-        fields={fields}
-      />
+      <TableFields formData={formData} fields={fields} />
+      {visible && (
+        <DatabaseTableModal
+          visible={visible}
+          dbname={dbname}
+          handleCancel={() => setVisible(false)}
+          handleSelect={handleTableSelect}
+          selectedRowKeys={selectedRowKeys}
+          {...props}
+        />
+      )}
     </Fragment>
   );
 }
---
+++ src/docs/relatedResource.md
@@ -0,0 +1,69 @@
+---
+title: 资源树多选
+order: 6
+nav:
+  title: 组件
+  order: 2
+---
+
+## RelatedResource 资源树多选
+
+基本使用:
+
+```jsx
+import React from 'react';
+import Demo from '../../src/DynamicFormItem/Demo';
+const schema = {
+  basicUse: {
+    title: '基本使用',
+    fields: [
+      {
+        name: 'resource',
+        label: '关联资源',
+        widget: 'resource',
+        rules: [
+          {
+            required: true,
+            message: '关联资源不能为空',
+          },
+        ],
+        options: {
+          placeholder: '请选择关联资源',
+          disabled: true,
+        },
+        fields: {
+          style: {
+            width: '100%',
+          },
+        },
+      },
+    ],
+  },
+};
+export default () => <Demo schema={schema} />;
+```
+
+## Props
+
+| 参数         | 说明               | 类型    | 默认值      | 是否必填 |
+| ------------ | ------------------ | ------- | ----------- | -------- |
+| name         | 表单项字段名       | string  | -           | true     |
+| label        | 表单项 label 标题  | string  | -           | true     |
+| defaultValue | 默认值             | string  | -           | false    |
+| widget       | 展示的组件类型     | string  | -           | true     |
+| required     | 是否必填           | boolean | false       | false    |
+| disabled     | 表单项组件是否可用 | boolean | -           | false    |
+| placeholder  | 表单项输入说明     | string  | -           | false    |
+| help         | 帮助信息           | string  | -           | false    |
+| rule         | 表单项验证规则     | array   | -           | false    |
+| style        | 表单项 ui 样式     | object  | width:"50%" | false    |
+| options      | 组件额外配置       | object  | -           | false    |
+
+## Option Props
+
+| 参数            | 说明                                                                                       | 类型    | 默认值 | 是否必填 |
+| --------------- | ------------------------------------------------------------------------------------------ | ------- | ------ | -------- |
+| formElementOpts | 设置具体表单元素的配置属性，例如 formElement: {placeholder: '',disabled: true,autoSize:{}} | object  | -      | false    |
+| disabledInEdit  | 在编辑时组件是否可用                                                                       | boolean | -      | false    |
+| uiWidget        | 自定义展示时需要渲染的 ui 组件                                                             | string  | -      | false    |
+| showLabel       | 在编辑时是否展示 label 说明                                                                | boolean | -      | false    |
